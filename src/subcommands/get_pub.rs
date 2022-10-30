c88d93ede6cb4dba6d939a3bc37c54a376860fd8
blkgoose <alessio.biancone@gmail.com>
2022-10-30 15:26:51 +0100

    refactor: split main into subcommands folder

---
+++ src/subcommands/get_pub.rs
@@ -0,0 +1,13 @@
+use super::{Error, Result};
+use crate::config::Config;
+
+pub fn handle(matches: &clap::ArgMatches, conf: Config) -> Result<()> {
+    let name: &String = matches
+        .get_one("key_name")
+        .ok_or(Error::CannotGetConfig("key_name".to_owned()))?;
+
+    match conf.keys.get(name) {
+        Some(user) => Ok(println!("{}", user.public_key)),
+        None => Err("ERROR: cannot find specified user")?,
+    }
+}
