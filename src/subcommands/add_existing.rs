82242c4f2f0ea3c23f448967d5a64d9c91b7ad54
blkgoose <alessio.biancone@gmail.com>
2022-11-08 13:04:33 +0100

    style: use clippy suggestions

--- src/subcommands/add_existing.rs
+++ src/subcommands/add_existing.rs
@@ -6,19 +6,19 @@ use crate::config::{Config, Key};
 pub fn handle(matches: &clap::ArgMatches, mut conf: Config) -> Result<()> {
     let name: &String = matches
         .get_one("key_name")
-        .ok_or(Error::CannotGetConfig("key_name".to_owned()))?;
+        .ok_or_else(|| Error::CannotGetConfig("key_name".to_owned()))?;
 
     let git_name: &String = matches
         .get_one("name")
-        .ok_or(Error::CannotGetConfig("name".to_owned()))?;
+        .ok_or_else(|| Error::CannotGetConfig("name".to_owned()))?;
 
     let git_email: &String = matches
         .get_one("email")
-        .ok_or(Error::CannotGetConfig("email".to_owned()))?;
+        .ok_or_else(|| Error::CannotGetConfig("email".to_owned()))?;
 
     let path: &PathBuf = matches
         .get_one::<PathBuf>("file")
-        .ok_or(Error::CannotGetConfig("key_name".to_owned()))?;
+        .ok_or_else(|| Error::CannotGetConfig("key_name".to_owned()))?;
 
     if conf.keys.contains_key(name) {
         Err("key_name exists already, delete it first if you want to overwrite it")?
--- src/subcommands/get_pub.rs
+++ src/subcommands/get_pub.rs
@@ -4,7 +4,7 @@ use crate::config::Config;
 pub fn handle(matches: &clap::ArgMatches, conf: Config) -> Result<()> {
     let name: &String = matches
         .get_one("key_name")
-        .ok_or(Error::CannotGetConfig("key_name".to_owned()))?;
+        .ok_or_else(|| Error::CannotGetConfig("key_name".to_owned()))?;
 
     match conf.keys.get(name) {
         Some(user) => Ok(println!("{}", user.public_key)),
